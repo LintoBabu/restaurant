44f1501792f164097eea941828e944cffcbda778
LINTO <lintob4@gmail.com>
2023-02-09 14:38:39 +0530

    updates

--- lib/pages/MessPage/MessDetails/MessValidity/messDetailsValidity.dart
+++ lib/pages/MessPage/MessDetails/MessValidity/messDetailsValidity.dart
@@ -33,72 +33,81 @@ class _messDetailsPackageState extends State<messDetailsPackage> {
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Card(
-              child: ListTile(
-                leading: Text("Linto"),
-                title: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text("Start Date:"),
-                        Text("10/01/2023"),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text("End Date: "),
-                        Text("10/02/2023"),
-                      ],
-                    )
-                  ],
-                ),
-                subtitle: Row(
-                  children: [
-                    Text("Status: "),
-                    Text("Completed: "),
-                  ],
-                ),
-                trailing: ElevatedButton.icon(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(buttonColors)),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => addMessPage()));
-                  },
-                  icon: Icon(Icons.autorenew),
-                  label: Text("Renew"),
+            child: Container(
+              child: Card(
+                elevation: 10,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      listreturn(
+                        "Customer Name",
+                        "JUBIN",
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      listreturn("Mess Start Date", "10/01/2023"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      listreturn("Mess End Date", "10/02/2023"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      listreturn("Status", "Completed"),
+                      Row(
+                        children: [
+                          Spacer(),
+                          ElevatedButton.icon(
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll(buttonColors)),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => addMessPage()));
+                            },
+                            icon: Icon(Icons.autorenew),
+                            label: Text("Renew"),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-          Card(
-            child: ListTile(
-              leading: Text("Jubin"),
-              title: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child: Card(
+                elevation: 10,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
                     children: [
-                      Text("Start Date:"),
-                      Text("08/02/2023"),
+                      listreturn(
+                        "Customer Name",
+                        "LINTO",
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      listreturn("Mess Start Date", "10/02/2023"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      listreturn("Mess End Date", "10/03/2023"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      listreturn("Status", "21 Days to go"),
                     ],
                   ),
-                  Row(
-                    children: [
-                      Text("End Date: "),
-                      Text(" 08/03/2023"),
-                    ],
-                  )
-                ],
-              ),
-              subtitle: Row(
-                children: [
-                  Text("Status: "),
-                  Text("27 days left"),
-                ],
+                ),
               ),
             ),
           ),
@@ -106,4 +115,35 @@ class _messDetailsPackageState extends State<messDetailsPackage> {
       ),
     );
   }
+}
+
+Widget listreturn(String text, String value) {
+  return Row(
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+            width: 150,
+            child: Text(
+              text,
+              style:
+                  GoogleFonts.lato(fontWeight: FontWeight.bold, fontSize: 18),
+            )),
+      ),
+      Text(
+        ': ',
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(
+        width: 20,
+      ),
+      Expanded(
+        child: Text(
+          value,
+          style: GoogleFonts.lato(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ),
+    ],
+  );
 }
